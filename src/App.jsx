562f4aef50e2f518660a98ac5e6f4d077293cbd4
Hasib Hossain <boraborhasib@gmail.com>
2023-03-23 22:38:58 +0600

    NavBar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <h1>Discover a New Era of Crypto Currency</h1>
+            <p>Panda NFT is the primier marketplace for NFT, which are digital items you can truly own. Digital items have existed for a long time, but never like this.</p>
+            <button>Get Started</button>
+        </div>
+    );
+};
+
+export default Hero;
