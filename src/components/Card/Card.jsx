54b0c63e2c4834b108d7c11e702ad73f7cd7631b
Hasib Hossain <boraborhasib@gmail.com>
2023-03-24 12:23:52 +0600

    Hero Done

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Card.css';
+import card_img from '../../assets/img1.jpg'
+import profile_img from '../../assets/profile.png'
+const Card = () => {
+    return (
+        <div>
+            <div className="card">
+                <div className="card_img">
+                    <img src={card_img} alt="" />
+                </div>
+                <div className="card_text">
+                    <h5>"Hamlet Contemplates ...</h5>
+                    <button>BSC</button>
+                </div>
+                <div className="profile">
+                    <div className="card_img">
+                        <div>
+                            <img src={profile_img} alt="" />
+                        </div>
+                        <div>
+                            <p>Creator</p>
+                            <h3>SalvadorDali</h3>
+                        </div>
+                    </div>
+                    <div className="card-sm-text">
+                        <p>Current Bid</p>
+                        <h4>4.89 ETH</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import './Hero.css';
 
+import Card from '../Card/Card';
 const Hero = () => {
     return (
-        <div>
-            <h1>Discover a New Era of Crypto Currency</h1>
-            <p>Panda NFT is the primier marketplace for NFT, which are digital items you can truly own. Digital items have existed for a long time, but never like this.</p>
-            <button>Get Started</button>
+        <div className='hero-section'>
+            <div className="hero-text">
+                <h1>Discover a New Era of Crypto Currency</h1>
+                <p>Panda NFT is the primier marketplace for NFT, which are digital items you can truly own. Digital items have existed for a long time, but never like this.</p>
+                <button>Get Started</button>
+            </div>
+            {/* Card Section  */}
+            <div className="card-high">
+                <Card></Card>
+            </div>
+            <div className="card-low">
+                <Card></Card>
+            </div>
+
         </div>
     );
 };
