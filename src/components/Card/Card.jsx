adfbe4c6501f1e14d237d67fbe71157ef814ec3a
Hasib Hossain <boraborhasib@gmail.com>
2023-03-24 14:17:24 +0600

    Love It

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './Card.css';
 import card_img from '../../assets/img1.jpg'
 import profile_img from '../../assets/profile.png'
+import fire_img from '../../assets/fire.png'
 const Card = () => {
     return (
         <div>
             <div className="card">
-                <div className="card_img">
-                    <img src={card_img} alt="" />
+                <div className="card_images">
+                    <div className="card-img">
+                        <img src={card_img} alt="" />
+                    </div>
+                    <div className="card-offer-img">
+                        <img src={fire_img} alt="" />
+                        <h3>05 : 12 : 07 : 45</h3>
+                    </div>
+
                 </div>
                 <div className="card_text">
                     <h5>"Hamlet Contemplates ...</h5>
